5bb16b7000b255ec6c09f304b737251283beb01f
andr33sdev <andresfcardoso1997@gmail.com>
2025-02-13 19:20:46 -0300

    user model created and sync with postgre

--- zentra-backend/src/database.ts
+++ zentra-backend/src/database.ts
@@ -6,6 +6,7 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.DB_URL!,
     {
         dialect: 'postgres',
+        logging: false, // Para evitar que se muestren los logs SQL
     }
 );
 
@@ -16,5 +17,10 @@ sequelize.authenticate()
     .catch((error) => {
         console.error('No se pudo conectar a la base de datos:', error);
     });
+
+// Sincronización con la base de datos
+sequelize.sync({ force: false }).then(() => { // force: false => significa que no elminará las tablas existentes (si las hubiera)
+    console.log('Base de datos sincronizada');
+});
 
 export default sequelize;
---
+++ zentra-backend/src/models/User.ts
@@ -0,0 +1,40 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../database";
+
+
+export class User extends Model {
+    public id!: number;
+    public username!: string;
+    public email!: string;
+    public password!: string;
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        modelName: 'User',
+        tableName: 'users',
+        timestamps: true
+    }
+)
